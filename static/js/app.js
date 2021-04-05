e576302680bcc0930b50af847dfe5f79b7c457fd
kaekacha <56618353+kaekacha@users.noreply.github.com>
2021-04-04 22:14:59 -0400

    updated

--- static/js/app.js
+++ static/js/app.js
@@ -75,3 +75,36 @@ button2.on("click", function(){
 // country
 // shape
 
+// EVENT LISTENER: filter table by date input
+var button3 = d3.select("#filter-btn");
+
+button1.on("click", function() {
+
+    // Select user input
+    var inputValue = d3.select("#datetime").property("value");
+
+    console.log(inputValue);
+    
+    // Filter table based on user input
+    var filteredData = tableData.filter(data => data.datetime === inputValue);
+    console.log(filteredData);
+
+    // Clear original table
+    tbody.html("");
+
+    // Write new table with forEach loop
+    filteredData.forEach(obj => {
+        
+        // Make new row
+        var row = tbody.append("tr");
+
+        // Append each row with data
+        row.append("td").text(obj.datetime);
+        row.append("td").text(obj.city);
+        row.append("td").text(obj.state);
+        row.append("td").text(obj.country);
+        row.append("td").text(obj.shape);
+        row.append("td").text(obj.durationMinutes);
+        row.append("td").text(obj.comments);
+    });
+});
